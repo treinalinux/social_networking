bbe585c9fcc79b046625667471c403b5f7b0810e
Alan da Silva Alves <linuxfama@gmail.com>
2021-03-30 11:12:52 -0300

    Rails - Panel Admin created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, skip: [:registrations]
+  # devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  namespace :admin do
+    root to: "home#index"
+    resources :admins
+    resources :users, only: :index
+  end
+
 end
