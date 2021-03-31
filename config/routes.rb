d5e1d678620c3a7a4a0a51c569faed3aae5eba98
Alan da Silva Alves <linuxfama@gmail.com>
2021-03-30 23:54:13 -0300

    Rails - Admin sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admins, skip: [:registrations]
-  # devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
+  # devise_for :admins, skip: [:registrations]
+  devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
